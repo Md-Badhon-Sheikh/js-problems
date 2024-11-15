c265750dece940b6f641a7f8834df35eeedfdad0
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-11-15 22:22:01 +0600

    added more files

---
+++ math.js
@@ -0,0 +1,11 @@
+const min = Math.min(18, 20, 30, 14, 55, 12, 45);
+// console.log(min);
+
+const max = Math.max(18, 20, 30, 14, 55, 12, 45);
+// console.log(max);
+
+// console.log(Math.PI);
+
+
+const lotary = (Math.random()*100);
+console.log(Math.round(lotary));
